7e5ade8a1c2da1b92ea7c37ea9aeaa252006476c
dmitrii <stepanenko.d@numdes.com>
2021-10-04 18:30:44 +0300

    add more tests

--- tests/test_scoping.py
+++ tests/test_scoping.py
@@ -1,5 +1,9 @@
+import json
+
 import scoring
 import pytest
+from datetime import datetime
+import hashlib
 
 
 @pytest.fixture
@@ -11,20 +15,25 @@ def store():
         def get(self, name):
             return self.cache.get(name)
 
-        def catch_get(self, name):
+        def cache_get(self, name):
             return self.get(name)
 
-        def set(self, name, value):
-            self.cache[name] = value
-
         def cache_set(self, name, value, time):
-            self.set(name, value)
+            ...
 
-    return Store()
+    cache = {
+        "uid:" + hashlib.md5("aab77777777777".encode('utf-8')).hexdigest(): 4,
+        "i:1": json.dumps(['a', 'b'])
+    }
+    return Store(cache)
 
 
 @pytest.mark.parametrize('phone, email, birthday, gender, first_name, last_name, answer', [
-    (77_777_777_777, 'a@b', '1.2.2000', 0, 'a', 'b', 5),
+    ('77777777777', 'a@b', datetime(2000, 1, 1), 1, 'a', 'b', 5),
+    ('77777777777', 'a@b', datetime(2000, 1, 1), 0, 'a', 'b', 3.5),
+    ('77777777777', 'a@b', datetime(2000, 1, 1), 1, '', 'b', 4.5),
+    ('77777777777', 'a@b', None, None, None, None, 3),
+    ('77777777777', 'a@b', None, 1, 'aa', 'b', 4),
 ])
 def test_get_score(store, phone, email, birthday, gender, first_name, last_name, answer):
     score = scoring.get_score(
@@ -37,3 +46,13 @@ def test_get_score(store, phone, email, birthday, gender, first_name, last_name,
         last_name=last_name)
 
     assert score == answer
+
+
+@pytest.mark.parametrize('cid, answer', [
+    (1, ['a', 'b']),
+    (2, [])
+])
+def test_get_interests(store, cid, answer):
+    interests = scoring.get_interests(store, cid)
+
+    assert interests == answer
